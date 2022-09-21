c0fb9f5212aee7571374bfb9c43c744c01d4e082
jwjin <wlsdn2316@naver.com>
2022-09-21 15:15:50 +0900

    불필요 부분 삭제

--- CentOS_Shell_Script.sh
+++ CentOS_Shell_Script.sh
@@ -165,39 +165,3 @@ case "$CASE_CHECK_1" in
         echo "잘못 선택했습니다."
         ;;        
 esac
-
-
-
-
-
-
-
-
-echo -e " IP 입력 "
-read DB_ENDPOINT
-
-echo -e " NETMASK입력 "
-read DB_MASTER
-
-echo -e " GATEWAY입력 "
-read DB_MASTER
-
-echo -e " DNS 입력 "
-read DB_PASSWORD
-
-sudo sed -i 's/jjwmasterdb.chabsh0zrlyl.ap-northeast-2.rds.amazonaws.com/'$DB_ENDPOINT'/' /server/was/instances/center/conf/server.xml
-sudo sed -i 's/jinwoo/'$DB_MASTER'/' /server/was/instances/center/conf/server.xml
-sudo sed -i 's/jt201wdb/'$DB_PASSWORD'/' /server/was/instances/center/conf/server.xml
-sudo yum install -y java-1.8.0-openjdk-devel.x86_64
-sudo yum install -y mysql
-sudo echo JAVA_HOME=/etc/alternatives/java_sdk >> /etc/profile 
-sudo echo 'CLASSPATH=.:$JAVA_HOME/lib/tools.jar' >> /etc/profile 
-sudo echo PATH='$PATH:$JAVA_HOME/bin' >> /etc/profile 
-sudo echo CATALINA_HOME=/server/was/tomcat8 >> /etc/profile 
-sudo echo export JAVA_HOME CLASSPATH PATH CATALINA_HOME >> /etc/profile 
-sudo echo 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CATALINA_HOME/lib' >> /etc/profile 
-sudo echo export LD_LIBRARY_PATH >> /etc/profile
-sudo useradd tomcat
-sudo chown -R tomcat:tomcat /server
-echo `mysql -u$DB_MASTER -p$DB_PASSWORD -h $DB_ENDPOINT < /alldatabases_2021-01-13.sql`
-su - tomcat -c /server/was/launcher/startup_center.sh
